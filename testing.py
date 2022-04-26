cb72e144b10a22b4a52e3069496795ed01ebae79
Muhammad fauzan <m19fauzan@gmail.com>
2022-04-27 00:20:31 +0700

    menambahkan file testing.py

---
+++ testing.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+#1D array
+a = np.array([[1, 3, 0], [2, 4, 1], [3, 6, 7]])
+print('banyaknya entry = ', a.size)
+print('dimensi array = ', a.ndim)
+print('ukuran array = ', a.shape)
